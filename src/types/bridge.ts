d3d8264161bcf91aca7ababc238ad516e6fd84e3
codingman <zhuxian@SKATTING-10131.local>
2021-08-25 17:00:01 +0800

    edit types file

--- src/types/bridge.ts
+++ src/types/bridge.ts
@@ -5,18 +5,18 @@ export interface CallbackArgs {
   init?: (arg: any) => void;
   registerHandler?: (
     method: string,
-    args: (arg1: unknown, arg2: (options: Record<string, any>) => void) => void
+    args: (arg1: any, arg2: (options: Record<string, any>) => void) => void
   ) => void;
   callHandler?: (
     method: string,
-    data: Record<string, unknown>,
-    callback: (option: unknown) => void
+    data: Record<string, any>,
+    callback: (option: any) => void
   ) => void;
 }
 export interface CallNativeOption {
   method: string;
-  data?: Record<string, unknown>;
-  callback?: (arg: unknown) => void;
+  data?: Record<string, any>;
+  callback?: (arg: any) => void;
 }
 export interface CustomWindow extends Window {
   WebViewJavascriptBridge: any;
